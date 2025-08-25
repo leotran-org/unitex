cc8099569fd5bcee60056304cf2e42f518f17e2e
qvanle <vanle199tk@gmail.com>
2025-08-25 21:41:46 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // IMPORTANT: for custom domain, keep base at root
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 }));
+
